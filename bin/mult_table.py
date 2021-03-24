fb3d87e99501c5fad99869449ad7f5139e1e46e3
Davide Albanese <davide.albanese@gmail.com>
2021-03-24 10:39:46 +0100

    Export query comment in mult_table

--- bin/mult_table.py
+++ bin/mult_table.py
@@ -24,10 +24,12 @@ for fn in glob.glob(os.path.join(SCREEN_DIR, "*")):
     screen_bn = os.path.basename(fn)
     screen_id = os.path.splitext(screen_bn)[0]
     screen_df = pd.read_table(fn, sep='\t', names=screen_header)
-    mult_series = screen_df.set_index("Query ID")["Median multiplicity"]
+    mult_series = screen_df \
+        .set_index(["Query ID", "Query comment"])["Median multiplicity"]
     mult_series.name = screen_id
     mult_list.append(mult_series)
 
 mult_df = pd.DataFrame(mult_list).T
 mult_df.fillna(0, inplace=True)
-mult_df.to_csv(MULT_OUT, sep='\t', float_format='%.0f', index_label="Query")
+mult_df.to_csv(MULT_OUT, sep='\t', float_format='%.0f',
+    index_label=["Query ID", "Query comment"])
